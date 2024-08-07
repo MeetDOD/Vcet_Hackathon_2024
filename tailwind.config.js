d9a236a0f4fbce2ba0f1efd32b41cd2d686fad9a
riteekagawand <riteekagawand7@gmail.com>
2024-08-07 13:28:30 +0530

    Add backend and dashboard folders; include Google Fonts in index CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bebas': ['Bebas Neue', 'sans-serif'],
+        'chakra': ['Chakra Petch', 'sans-serif'],
+        'exo': ['Exo 2', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+        'zilla': ['Zilla Slab', 'serif'],
+        'oswald': ['Oswald', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
