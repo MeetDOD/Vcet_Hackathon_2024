44d2f24546a2bc61bc2d93426abb5914e811ae68
meet <dodiyameet041@gmail.com>
2024-08-07 22:44:16 +0530

    Added navbar design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import StarsCanvas from '../components/StarBackground';
+import Hero from '../components/Hero';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
     return (
         <div className="relative w-full h-screen">
             <StarsCanvas />
             <div className="relative z-10">
+                <Navbar />
+                <Hero />
             </div>
         </div>
     );
