2e01802ea1de9c80bbffb6eb57927672955e83ee
riteekagawand <riteekagawand7@gmail.com>
2024-08-08 19:25:09 +0530

    Make About Us page responsive with Tailwind CSS adjustments

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -7,15 +7,15 @@ const About = () => {
   return (
     <div className="bg-black min-h-screen text-white">
       <div className='flex justify-center'>
-        <p className="text-[60px] font-orbitron font-bold mt-5 text-[#d88a4c]">About Us</p>
+        <p className="text-[40px] md:text-[60px] font-orbitron font-bold mt-5 text-[#d88a4c]">About Us</p>
       </div>
-      <div className='flex items-center mt-10'>
-        <div className="relative ml-[230px]">
-          <img src={ImageScale} alt="Moon Sign" className="h-[450px] w-[450px] absolute top-0 left-0 rotate-animation" />
-          <img src={ImageRotate} alt="Moon Sign" className="h-[400px] w-[400px] absolute top-0 left-0 mt-7 ml-5 counter-rotate-animation" />
-          <img src={Image1} alt="Moon Inverse" className="h-[450px] w-[450px]" />
+      <div className='flex flex-col md:flex-row items-center mt-10 px-5 md:px-0'>
+        <div className="relative md:ml-[230px]">
+          <img src={ImageScale} alt="Moon Sign" className="h-[250px] w-[250px] md:h-[450px] md:w-[450px] absolute top-0 left-0 rotate-animation" />
+          <img src={ImageRotate} alt="Moon Sign" className="h-[220px] w-[220px] md:h-[400px] md:w-[400px] absolute top-0 left-0 mt-4 md:mt-7 ml-3 md:ml-5 counter-rotate-animation" />
+          <img src={Image1} alt="Moon Inverse" className="h-[250px] w-[250px] md:h-[450px] md:w-[450px]" />
         </div>
-        <div className='w-[700px] text-[23px] ml-24 font-exo text-justify text-[#999898]'>
+        <div className='w-full md:w-[700px] text-[18px] md:text-[23px] mt-5 md:mt-0 md:ml-24 font-exo text-justify text-[#999898]'>
           <p>
             VCET Hackathon is a 30-hour hackathon organized by the Department of Information Technology of <span className='text-[#d88a4c]'>"Vidyavardhini&apos;s College of Engineering and Technology".</span> By organizing this hackathon, we aim to promote a strong programming and product-building culture among students that will help them develop problem-solving, critical thinking, and software development skills. It is an opportunity to tackle challenging problems that affect us all and find solutions. Will you get time to eat? Can you ditch your sleep? Do you have a passion for building? Find out in this 30-hour long thrilling experience!
           </p>
