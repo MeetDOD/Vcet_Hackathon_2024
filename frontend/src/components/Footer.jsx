c8f87df3b1df51a90acf500380dc5446cb62c776
meet <dodiyameet041@gmail.com>
2024-08-12 11:47:39 +0530

    Footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -22,50 +22,49 @@ const quickLinks = [
 
 const Footer = () => {
     return (
-    
         <>
-            <footer className="bg-black px-8 pt-10 pb-5 mt-7 w-full">
+            <footer className="bg-gray-950 px-8 pt-10 pb-5 mt-7 w-full font-montserrat">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-screen-2xl mx-auto">
-                    
+
                     {/* Logo and Follow Us On Section */}
                     <div className="flex flex-col items-center md:items-start p-4">
-                    <img src={logo} alt="Logo" className="w-full max-w-xs mb-4" />
-                    <h2 className="text-[#cbb79c] font-bold text-xl mb-4">Follow Us On</h2>
-                    <ul className="flex list-none p-0">
-                        {socialLinks.map((link) => (
-                        <li key={link.id} className="mx-2.5">
-                            <a href={link.url} target="_blank" className="text-[#cbb79c] text-[1.5rem] transition-transform duration-500 hover:scale-110">
-                            <link.icon />
-                            </a>
-                        </li>
-                        ))}
-                    </ul>
+                        <img src={logo} alt="Logo" className="w-full max-w-xs mb-4" />
+                        <h2 className="text-customOrange font-bold text-xl mb-4">Follow Us On</h2>
+                        <ul className="flex list-none p-0 gap-2">
+                            {socialLinks.map((link) => (
+                                <li key={link.id} className=" hover:-translate-y-1 transition-transform duration-300 ">
+                                    <a href={link.url} target="_blank" className="text-white text-2xl">
+                                        <link.icon />
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
 
                     {/* Quick Links Section */}
                     <div className="p-4">
-                    <h2 className="text-[#cbb79c] font-bold text-3xl mb-4">Quick Links</h2>
-                    <ul className="list-none p-0">
-                        {quickLinks.map((link) => (
-                        <li key={link.id} className="mb-2.5">
-                            <HashLink to={`/#${link.id}`} className="text-white text-[1rem] font-normal relative before:content-[''] before:absolute before:bottom-[-2px] before:left-0 before:w-0 before:h-[2px] before:bg-white hover:before:w-full before:transition-all before:duration-300 hover:text-[#F4CA93]">
-                            {link.label}
-                            </HashLink>
-                        </li>
-                        ))}
-                    </ul>
+                        <h2 className="text-customOrange font-bold text-3xl mb-4">Quick Links</h2>
+                        <ul className="list-none p-0">
+                            {quickLinks.map((link) => (
+                                <li key={link.id} className="mb-2.5">
+                                    <HashLink to={`/#${link.id}`} className="text-white text-[1rem] font-normal relative before:content-[''] before:absolute before:bottom-[-2px] before:left-0 before:w-0 before:h-[2px] before:bg-white hover:before:w-full before:transition-all before:duration-300 hover:text-customOrange">
+                                        {link.label}
+                                    </HashLink>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
 
                     {/* About Hackathon Section */}
                     <div className="p-4">
-                    <h2 className="text-[#cbb79c] font-bold text-3xl mb-4">About Hackathon</h2>
-                    <p className="text-white text-[1rem] font-normal ">
-                        Hackathons help IT companies find talented Programmers, Software Developers, Designers, etc. They also help participants launch successful startups if their products or solutions have business value. So take up the challenge and code your way to the top!
-                    </p>
+                        <h2 className="text-customOrange font-bold text-3xl mb-4">About Hackathon</h2>
+                        <p className="text-white text-[1rem] font-normal ">
+                            Hackathons help IT companies find talented Programmers, Software Developers, Designers, etc. They also help participants launch successful startups if their products or solutions have business value. So take up the challenge and code your way to the top!
+                        </p>
                     </div>
                 </div>
 
-                <div className="text-center text-sm text-[#d7be9e] mt-7 pt-4 mb-2.5">
+                <div className="text-center text-sm text-white font-semibold mt-7 pt-4 mb-2.5">
                     Copyright © 2023 All rights reserved by Vidyavardhini's College Of Engineering & Technology
                 </div>
             </footer>
