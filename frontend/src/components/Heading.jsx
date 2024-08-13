bfb8c1f3eeb355dabbbec37fd355834d45295789
meet <dodiyameet041@gmail.com>
2024-08-13 23:39:27 +0530

    Made heading responsive

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -22,10 +22,6 @@ const Heading = ({ title1, title2, subtitle, sectionId }) => {
             { width: '0%', opacity: 0 },
             { width: '100%', opacity: 1, duration: 1, ease: "power1.inOut" }
         )
-            .fromTo('.rocket-icon',
-                { x: 0, opacity: 0 },
-                { x: '100%', opacity: 1, duration: 1, ease: "power1.inOut" }, 0
-            );
     }, [sectionId]);
 
     const defaultOptions = {
@@ -39,10 +35,10 @@ const Heading = ({ title1, title2, subtitle, sectionId }) => {
 
     return (
         <div id={sectionId} className="flex flex-col justify-center items-center py-[72px] z-10 heading-section">
-            <h2 className="font-semibold text-5xl md:text-6xl text-center tracking-tighter relative inline-block">
+            <h2 className="font-semibold  text-3xl md:text-4xl lg:text-5xl text-center tracking-tighter relative inline-block">
                 {title1}
-                <span className='rocket-icon inline-block align-middle -ml-16 mr-16 pb-3'>
-                    <Lottie options={defaultOptions} height={70} width={70} />
+                <span className='rocket-icon inline-block align-middle pb-3 h-14 md:h-20'>
+                    <Lottie options={defaultOptions} />
                 </span>
                 <span className='text-customOrange'>{title2}</span>
                 <div className='underline block h-[2.5px] bg-customOrange absolute bottom-0 left-0 w-full shadow-[0px_0.5px_5px_#F5AF64]'></div>
